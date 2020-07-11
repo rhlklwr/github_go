a14f5185081c44e413bb3af2cf14eefae42d6dbb
rhlklwr <kalwar.rahul@gmail.com>
2020-07-11 17:54:00 +0530

    GO

--- Temp/Temp.go
+++ Temp/Temp.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-const (
-	winter = 1
-	summer = 3
-	yearly = winter + summer
-)
-
 func main() {
-	blue := []int{
-		4, 5, 6, 7, 8, 10,
-	}
-	blue = append(blue, 8, 9, 10, 11, 12)
-	fmt.Printf("blue: %#v\n%v\n%v", blue, cap(blue), len(blue))
+	println()
 }

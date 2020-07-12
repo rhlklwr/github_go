6bdaabd433dcfc252f07eff47b68dfbd6e992043
rhlklwr <kalwar.rahul@gmail.com>
2020-07-12 13:28:00 +0530

    GO

--- Temp/Temp.go
+++ Temp/Temp.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
+	"time"
 )
 
 func main() {
-	const text = `Galaksinin Batı Sarmal Kolu'nun bir ucunda, haritası bile çıkarılmamış ücra bir köşede, gözlerden uzak, küçük ve sarı bir güneş vardır.
-
-Bu güneşin yörüngesinde, kabaca yüz kırksekiz milyon kilometre uzağında, tamamıyla önemsiz ve mavi-yeşil renkli, küçük bir gezegen döner.
-
-Gezegenin maymun soyundan gelen canlıları öyle ilkeldir ki dijital kol saatinin hâlâ çok etkileyici bir buluş olduğunu düşünürler.`
-
-	r, size := utf8.DecodeRuneInString("öykü")
-	fmt.Printf("rune: %c size: %d bytes.\n", r, size)
-
+	fmt.Println(time.Second)
 }

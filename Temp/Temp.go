4c52dffd8eff0f4ec9f356a055c8b1cd73973351
rhlklwr <kalwar.rahul@gmail.com>
2020-07-18 16:49:58 +0530

    GO

--- Temp/Temp.go
+++ Temp/Temp.go
@@ -2,16 +2,59 @@ package main
 
 import "fmt"
 
+type printer interface {
+	print()
+	discount()
+}
+
+type book struct {
+	name  string
+	price int
+}
+
+type game struct {
+	name  string
+	price float64
+}
+
 func main() {
 
-	var a = map[string]int{"A": 1}
-	fmt.Println(a)
-	var b = map[string]int{"A": 5}
+	var book1 = book{
+		name:  "Moby",
+		price: 5,
+	}
 
-	c := a
+	var game1 = game{
+		name:  "GTA",
+		price: 5.5,
+	}
 
-	c["A"] = 6
+	type list []printer
 
-	fmt.Println(a, b, c)
+	var z list
 
+	z = append(z, book1, game1)
+
+	game1.discount()
+
+	for _, h := range z {
+		h.print()
+		h.discount()
+	}
+}
+
+func (b book) print() {
+	fmt.Printf("Book name is %v\nBook price is %v\n", b.name, b.price)
+}
+
+func (g game) print() {
+	fmt.Printf("Game name is %v\nGame price is %v\n", g.name, g.price)
+}
+
+func (g game) discount() {
+	fmt.Printf("Game name is %v\nGame price is %v\n", g.name, g.price-10)
+}
+
+func (b book) discount() {
+	fmt.Printf("Game name is %v\nGame price is %v\n", b.name, b.price-10)
 }
